4d09eef87fa7071fc02649e73ca1e1b515e2e7d7
Adeveloper <ademnurhadi@gmail.com>
2024-07-19 08:27:52 +0700

    p

--- resources/views/admin/user-management.blade.php
+++ resources/views/admin/user-management.blade.php
@@ -33,11 +33,12 @@
                                         <tr>
                                             <td>
                                                 <div class="d-flex px-2 py-1">
-                                                    <img src="{{ asset('storage/' . $user->profile_picture) }}" class="avatar avatar-sm me-3" alt="user1">
+                                                    <img src="{{ url('storage/' . $user->profile_picture) }}" class="avatar avatar-sm me-3" alt="user1">
                                                     <div class="d-flex flex-column justify-content-center">
                                                         <a href="javascript:;" class="mb-0 text-sm">{{ $user->name }}</a>
                                                     </div>
                                                 </div>
+
 
                                             </td>
                                             <td>
